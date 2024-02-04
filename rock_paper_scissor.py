e1a4760543b8e5320c7b074443b72f14db709eb8
Gayathri Krishnan <gayathri.krishnan.designer@gmail.com>
2024-02-04 21:47:41 +0530

    Didn't call the Function

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -18,3 +18,5 @@ def is_win(player, opponent):
     # r > s, s > p, p > r
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+
+print(play())
